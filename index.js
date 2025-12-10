3b9b7452e1297bbdfb2d8934065f5c092f5b99b0
kyrokobebolt <rayraya2015@gmail.com>
2025-12-10 21:48:04 +0530

    Added the three main files
    html, css, js

---
+++ index.js
@@ -0,0 +1,14 @@
+let firstCard = 11
+let secondCard = 11
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw?")
+} else if (sum === 21) {
+    console.log("BLACKJACK-pot!!!")
+} else {
+    console.log("Your're out of the game.")
+}
+
+
+
